15c67ce406bfdce8b3b3a808b8debb557bee09dc
bhavan895 <bpatel4@ggc.edu>
2016-04-06 13:17:33 -0700

    Update bpseed.rb

--- bpseed.rb
+++ bpseed.rb
@@ -1,1 +1,25 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Requestb.create(:title => "10-Yard Fight", :link => "http://www.nintendonerds.com/games/categories/nes/10-yard-fight-r1",
+    :publisher => "Nintendo", :year => "October 18, 1985", :genre => "Sports", :players => "Single Player, 2 Player Simultaneous")
+
+#Payment methord 
+
+Paymentmethordbp.create(:title =>"Payment method options", :name_of_the_card "MC", :Credit_Card_Number "4444-3333-2222-1111" )
+
+#Verfication 
+
+#Verifing process 
+
+#Requests
+
+#Summery 
+
+#User Information 
 
